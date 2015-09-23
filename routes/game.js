d291da78a421b046862df84b185d467150976a5d
Andrew Mockett <califormula@gmail.com>
2015-09-22 21:36:30 -0600

    some computer logic

--- routes/game.js
+++ routes/game.js
@@ -12,6 +12,7 @@ var db = require('monk')('localhost/hands');
 var hands = db.get('hands')
 
 
+
 router.get('/', function(req, res, next) {
   res.render('game', { title: 'Rock Paper Scissors', user: req.user });
 });
@@ -20,9 +21,47 @@ router.post('/', function(req, res){
   hands.update({user: req.user.username}, {
     $push: {userChoice: req.body.userChoice, cpuChoice: req.body.cpuChoice}
   }, function(){
-    // run computer choice logic here as a JSON ex: {computer choice: "rock"}
-    var myCursor = hands.find({user: req.user.username}, {"_id": 0, "userChoice": 1}, function(err, data){
-      console.log(myCursor.toArray());
+    // run computer choice logic here and output a JSON ex: {computer choice: "rock"}
+    hands.find({user: req.user.username}, {"_id": 0, "userChoice": 1}, function(err, data){
+      var userArray = data[0]["userChoice"];
+      userArray.pop();
+      var recentChoices = userArray.slice(-3);
+      var choices = [];
+      for (var i = 0; i < userArray.length; i++){
+        if (recentChoices[0] === userArray[i] && recentChoices[1] === userArray[i+1] && recentChoices[2] === userArray[i+2]){
+          if (userArray[i+3] != undefined){
+            choices.push(userArray[i+3])
+          }
+        }
+      }
+      console.log(choices);
+      var choicesObj = choices.reduce(function(prev, curr){
+        if (prev[curr]){
+          prev[curr]++;
+          return prev;
+        } else {
+          prev[curr] = 1;
+          return prev;
+        }
+      }, {})
+      console.log(choicesObj);
+      var max = 0;
+      var choice;
+      for (prop in choicesObj){
+        if (choicesObj[prop] > max){
+          max = choicesObj[prop];
+          choice = prop;
+        }
+      }
+      var cpuChoice = {}; 
+      if (choice === "Rock"){
+        cpuChoice.choice = "Paper";
+      } else if (choice === "Paper"){
+        cpuChoice.choice = "Scissors";
+      } else if (choice === "Scissors"){
+        cpuChoice.choice = "Rock";
+      }
+      console.log(cpuChoice);
     });
   })
 })
