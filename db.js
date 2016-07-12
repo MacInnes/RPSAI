f79f037ffe41761626bfa2f0a148759fa57646cb
Andrew Mockett <califormula@gmail.com>
2016-07-12 16:38:43 -0600

    PLEEEASE

--- db.js
+++ db.js
@@ -6,3 +6,9 @@ function connect(){
   db = monk(process.env.MONGODB_URI || 'localhost/hands');
   mongoose.connect(process.env.MONGODB_URI || 'localhost/hands');
 }
+
+function get(collection){
+  return db.get(collection);
+}
+
+module.exports = {'connect': connect, 'get': get};
