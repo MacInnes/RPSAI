1648cb7ae5ecaa5be0b3fc2de23b28f53d0c1d3d
MacInnes <califormula@gmail.com>
2016-07-13 15:11:48 -0600

    more env variable edits

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var db;
 module.exports = {
 	connect: function(){
 	  db = monk(process.env.MONGODB_URI || 'localhost/hands');
-	  mongoose.connect(process.env.MONGODB_URI || 'localhost/hands');
+	  mongoose.connect('mongodb://' + process.env.MONGODB_URI || 'localhost/hands');
 	},
 	get: function(collection){
 	  return db.get(collection);
