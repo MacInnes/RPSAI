229db922f0dbbbe177374e26418568012da6592c
Andrew Mockett <califormula@gmail.com>
2016-07-13 16:36:14 -0600

    removed monk in code, package still there

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
-var monk = require('monk');
+// var monk = require('monk');
 var db;
 
 module.exports = {
 	connect: function(){
 	  mongoose.connect(process.env.MONGODB_URI || 'localhost/hands');
-    db = monk('mongodb://' + process.env.MONGODB_URI);
+    db = mongoose.connection;
 	  
 	},
 	get: function(collection){
