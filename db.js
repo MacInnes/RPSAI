8921fe743cc1030115e760127a80cd9b4e8e8024
MacInnes <califormula@gmail.com>
2016-07-14 13:40:58 -0600

    still debugging

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 // var monk = require('monk');
+var Hands = require('../models/hands');
 var db;
 
 module.exports = {
 	connect: function(){
 	  mongoose.connect(process.env.MONGODB_URI || 'localhost/hands');
     db = mongoose.connection;
-	  
-	},
-	get: function(collection){
-	  return db.find(collection);
 	}
+	// get: function(collection){
+	//   return db.find(collection);
+	// }
 };
--- models/hands.js
+++ models/hands.js
@@ -2,8 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var hands = new Schema({
+	user: String,
+	userChoice: [String],
+	compChoice: [String],
+	result: [String]
+});
 
-})
 
-
-module.exports = mongoose.model('hands', hands);
+module.exports = mongoose.model('Hands', hands);
